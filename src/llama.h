02a2edfdba82562da3ff30af4b8de366d04c6817
harry-martin <up896106@myport.ac.uk>
2021-08-19 18:03:31 +0100

    add basic angle conversion macros

--- src/llama.h
+++ src/llama.h
@@ -5,4 +5,9 @@
 #define true 1
 #define false 0
 
+#define PI 3.14159265359
+
+#define DEGREES(x) (x * (180.0f / PI))
+#define RADIANS(x) (x * (PI / 180))
+#define LOGF(x) (printf("%f\n", x))
 #endif
